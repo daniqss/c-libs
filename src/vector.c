090de7024fbc672d0806b374d7cc7faf4d05bbff
daniqss <danielqueijo14@gmail.com>
2024-07-28 18:14:22 +0200

    pass new test for new implementation

--- src/vector.c
+++ src/vector.c
@@ -20,37 +20,33 @@ int32_t vector_new(Vector *vector, uint64_t element_size) {
     return vector_alloc(vector, VECTOR_SIZE, element_size);
 }
 
-int32_t vector_from(Vector *vector, void *data, uint64_t length, uint64_t element_size) {
-    int32_t result = vector_alloc(vector, length, element_size);
-    void *element = NULL;
+// int32_t vector_from(Vector *vector, void *data, uint64_t length, uint64_t element_size) {
+//     int32_t result = vector_alloc(vector, length, element_size);
+//     void *element = NULL;
 
-    if (result != SUCCESS) return result;
-    for (uint64_t i = 0; i < length; i++) {
-        result = copy_memory(data + i * element_size, &element, element_size);
-        if (result != SUCCESS) {
-            for (uint64_t j = 0; j < i; j++) {
-                free((*vector)->data[j]);
-            }
-            free((*vector)->data);
-            return result;
-        }
-        (*vector)->data[i] = element;
-        element = NULL;
-    }
-    (*vector)->length = length;
+//     if (result != SUCCESS) return result;
+//     for (uint64_t i = 0; i < length; i++) {
+//         result = copy_memory(data + i * element_size, &element, element_size);
+//         if (result != SUCCESS) {
+//             for (uint64_t j = 0; j < i; j++) {
+//                 free((*vector)->data[j]);
+//             }
+//             free((*vector)->data);
+//             return result;
+//         }
+//         (*vector)->data[i] = element;
+//         element = NULL;
+//     }
+//     (*vector)->length = length;
 
-    return SUCCESS;
-}
+//     return SUCCESS;
+// }
 
-int32_t vector_with_capacity(Vector *vector, uint64_t capacity, uint64_t element_size) {
-    return vector_alloc(vector, capacity, element_size);
-}
+// int32_t vector_with_capacity(Vector *vector, uint64_t capacity, uint64_t element_size) {
+//     return vector_alloc(vector, capacity, element_size);
+// }
 
 int32_t vector_delete(Vector *vector) {
-    for (uint64_t i = 0; i < (*vector)->length; i++) {
-        // vector_pop(*vector);
-        free((*vector)->data[i]);
-    }
     free((*vector)->data);
     free(*vector);
     *vector = NULL;
@@ -58,24 +54,24 @@ int32_t vector_delete(Vector *vector) {
     return SUCCESS;
 }
 
-int32_t vector_push(Vector vector, void *element) {
-    void *element_cpy = NULL;
+// int32_t vector_push(Vector vector, void *element) {
+//     void *element_cpy = NULL;
 
-    if (vector->length + 1 >= vector->capacity)
-        if (vector_realloc(vector) == ERROR_MALLOC)
-            return ERROR_MALLOC;
+//     if (vector->length + 1 >= vector->capacity)
+//         if (vector_realloc(vector) == ERROR_MALLOC)
+//             return ERROR_MALLOC;
 
-    int32_t result = copy_memory(element, &element_cpy, vector->element_size);
-    if (result != SUCCESS) return result;
+//     int32_t result = copy_memory(element, &element_cpy, vector->element_size);
+//     if (result != SUCCESS) return result;
 
-    vector->data[vector->length++] = element_cpy;
-    return SUCCESS;
-}
+//     vector->data[vector->length++] = element_cpy;
+//     return SUCCESS;
+// }
 
-int32_t vector_pop(Vector vector) {
-    free(vector->data[--vector->length]);
-    return SUCCESS;
-}
+// int32_t vector_pop(Vector vector) {
+//     free(vector->data[--vector->length]);
+//     return SUCCESS;
+// }
 
 uint64_t vector_length(Vector vector) {
     return vector->length;
@@ -85,53 +81,53 @@ uint64_t vector_capacity(Vector vector) {
     return vector->capacity;
 }
 
-int32_t vector_at(Vector vector, const void **element, uint64_t index) {
-    if (vector == NULL) return ERROR_ARGS;
-    index = index % vector->length;
+// int32_t vector_at(Vector vector, const void **element, uint64_t index) {
+//     if (vector == NULL) return ERROR_ARGS;
+//     index = index % vector->length;
 
-    *element = vector->data[index];
-    return SUCCESS;
-}
+//     *element = vector->data[index];
+//     return SUCCESS;
+// }
 
-int32_t vector_clone_at(Vector vector, void **element, uint64_t index) {
-    if (vector == NULL) return ERROR_ARGS;
-    index = index % vector->length;
+// int32_t vector_clone_at(Vector vector, void **element, uint64_t index) {
+//     if (vector == NULL) return ERROR_ARGS;
+//     index = index % vector->length;
 
-    int32_t result = copy_memory(vector->data[index], element, vector->element_size);
-    return result;
-}
+//     int32_t result = copy_memory(vector->data[index], element, vector->element_size);
+//     return result;
+// }
 
-int32_t vector_iter(Vector vector, void (*fn)(void *vector_element, void *args), void *args) {
-    if (vector == NULL) return ERROR_ARGS;
-    for (uint64_t i = 0; i < vector->length; i++)
-        fn(vector->data[i], args);
+// int32_t vector_iter(Vector vector, void (*fn)(void *vector_element, void *args), void *args) {
+//     if (vector == NULL) return ERROR_ARGS;
+//     for (uint64_t i = 0; i < vector->length; i++)
+//         fn(vector->data[i], args);
 
-    return SUCCESS;
-}
+//     return SUCCESS;
+// }
 
-int32_t vector_map(Vector vector, Vector *new_vector, uint64_t new_element_size, void *(*fn)(void *vector_element, void *args), void *args) {
-    void **new_array = NULL;
-    void *element = NULL;
+// int32_t vector_map(Vector vector, Vector *new_vector, uint64_t new_element_size, void *(*fn)(void *vector_element, void *args), void *args) {
+//     void **new_array = NULL;
+//     void *element = NULL;
     
-    if (vector == NULL) return ERROR_ARGS;
-    if ((new_array = malloc(vector->length * sizeof(void *))) == NULL) return ERROR_MALLOC;
-    for (uint64_t i = 0; i < vector->length; i++) {
-        // The given function should return a pointer to heap-allocated memory
-        element = fn(vector->data[i], args);
-        if (element == NULL) {
-            for (uint64_t j = 0; j < i; j++) {
-                free(new_array[j]);
-            }
-            free(new_array);
-            return ERROR_MALLOC;
-        }
+//     if (vector == NULL) return ERROR_ARGS;
+//     if ((new_array = malloc(vector->length * sizeof(void *))) == NULL) return ERROR_MALLOC;
+//     for (uint64_t i = 0; i < vector->length; i++) {
+//         // The given function should return a pointer to heap-allocated memory
+//         element = fn(vector->data[i], args);
+//         if (element == NULL) {
+//             for (uint64_t j = 0; j < i; j++) {
+//                 free(new_array[j]);
+//             }
+//             free(new_array);
+//             return ERROR_MALLOC;
+//         }
 
-        new_array[i] = element;
-    }
-    vector_from(new_vector, new_array, vector->length, new_element_size);
+//         new_array[i] = element;
+//     }
+//     vector_from(new_vector, new_array, vector->length, new_element_size);
 
-    return SUCCESS;
-}
+//     return SUCCESS;
+// }
 
 // Private Functions
 int32_t vector_alloc(Vector *vector, uint64_t capacity, uint64_t element_size) {
@@ -142,16 +138,15 @@ int32_t vector_alloc(Vector *vector, uint64_t capacity, uint64_t element_size) {
     (*vector)->capacity = capacity;
     (*vector)->element_size = element_size;
 
-    if (((*vector)->data = (void **)malloc((*vector)->capacity * sizeof(void *))) == NULL)
+    if (((*vector)->data = (uint8_t *)malloc((*vector)->capacity * sizeof(uint8_t))) == NULL)
         return ERROR_MALLOC;
 
     return SUCCESS;
 }
 
 int32_t vector_realloc(Vector vector) {
-    void **aux = NULL;
-
-    aux = realloc(vector->data, (vector->capacity + VECTOR_SIZE) * sizeof(void *));
+    uint8_t *aux = realloc(vector->data, (vector->capacity + VECTOR_SIZE) * sizeof(uint8_t));
+    
     if (aux == NULL)
         return ERROR_MALLOC;
 
--- test/vector_test.c
+++ test/vector_test.c
@@ -12,140 +12,154 @@
 #define TEST_SMALL_VECTOR 32
 #define TEST_LARGE_VECTOR 1024 
 
+typedef struct {
+    int64_t x;
+    int64_t y;
+    int64_t z;
+} Point;
+
 static void test_vector_new() {
-    Vector vector = NULL;
-    int32_t result = vector_new(&vector, sizeof(int));
+    Vector vector1 = NULL;
+    Vector vector2 = NULL;
+    int32_t result1 = vector_new(&vector1, sizeof(int));
+    int32_t result2 = vector_new(&vector2, sizeof(Point));
 
-    assert_int_equal(result, SUCCESS);
-    assert_int_equal(vector_capacity(vector), 1024);
-    assert_int_equal(vector_length(vector), 0);
+    assert_int_equal(result1, SUCCESS);
+    assert_int_equal(vector_capacity(vector1), 1024);
+    assert_int_equal(vector_length(vector1), 0);
 
-    vector_delete(&vector);
-    assert_null(vector);
+    assert_int_equal(result2, SUCCESS);
+    assert_int_equal(vector_capacity(vector2), 1024);
+    assert_int_equal(vector_length(vector2), 0);
+
+    vector_delete(&vector1);
+    assert_null(vector1);
+    vector_delete(&vector2);
+    assert_null(vector2);
 }
 
-static void test_vector_push() {
-    Vector vector = NULL;
-    uint32_t size = 5;
+// static void test_vector_push() {
+//     Vector vector = NULL;
+//     uint32_t size = 5;
 
 
-    int32_t result = vector_with_capacity(&vector, size, sizeof(int32_t));
-    assert_int_equal(result, SUCCESS);
-    assert_int_equal(vector_capacity(vector), size);
-    assert_int_equal(vector_length(vector), 0);
+//     int32_t result = vector_with_capacity(&vector, size, sizeof(int32_t));
+//     assert_int_equal(result, SUCCESS);
+//     assert_int_equal(vector_capacity(vector), size);
+//     assert_int_equal(vector_length(vector), 0);
 
-    size = 10;
+//     size = 10;
 
-    for (uint32_t i = 0; i < size; i++) {
-        result = vector_push(vector, &i);
-        assert_int_equal(result, SUCCESS);
-        assert_int_equal(vector_length(vector), i + 1);
-    }
+//     for (uint32_t i = 0; i < size; i++) {
+//         result = vector_push(vector, &i);
+//         assert_int_equal(result, SUCCESS);
+//         assert_int_equal(vector_length(vector), i + 1);
+//     }
 
-    for (uint32_t i = 0; i < size; i++) {
-        result = vector_pop(vector);
-        assert_int_equal(result, SUCCESS);
-        assert_int_equal(vector_length(vector), size - i - 1);
-    }
+//     for (uint32_t i = 0; i < size; i++) {
+//         result = vector_pop(vector);
+//         assert_int_equal(result, SUCCESS);
+//         assert_int_equal(vector_length(vector), size - i - 1);
+//     }
 
-    vector_delete(&vector);
-    assert_null(vector);
-}
+//     vector_delete(&vector);
+//     assert_null(vector);
+// }
 
-static void test_vector_at() {
-    Vector vector = NULL;
-    int64_t array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-    };
-    int8_t *element = NULL;
-    const void *ptr;
-    int64_t result[TEST_SMALL_VECTOR];
+// static void test_vector_at() {
+//     Vector vector = NULL;
+//     int64_t array[] = {
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+//     };
+//     int8_t *element = NULL;
+//     const void *ptr;
+//     int64_t result[TEST_SMALL_VECTOR];
 
-    assert_int_equal(vector_from(&vector, array, TEST_SMALL_VECTOR, sizeof(int64_t)), SUCCESS);
-    assert_int_equal(vector_capacity(vector), TEST_SMALL_VECTOR);
-    assert_int_equal(vector_length(vector), TEST_SMALL_VECTOR);
+//     assert_int_equal(vector_from(&vector, array, TEST_SMALL_VECTOR, sizeof(int64_t)), SUCCESS);
+//     assert_int_equal(vector_capacity(vector), TEST_SMALL_VECTOR);
+//     assert_int_equal(vector_length(vector), TEST_SMALL_VECTOR);
 
-    for (uint32_t i = 0; i < TEST_SMALL_VECTOR; i++) {
-        result[i] = vector_clone_at(vector, (void **)&element, i);
-        assert_int_equal(result[i], SUCCESS);
-        assert_int_equal(*element, array[i]);
-        free(element);
-    }
+//     for (uint32_t i = 0; i < TEST_SMALL_VECTOR; i++) {
+//         result[i] = vector_clone_at(vector, (void **)&element, i);
+//         assert_int_equal(result[i], SUCCESS);
+//         assert_int_equal(*element, array[i]);
+//         free(element);
+//     }
 
-    for (uint32_t i = 0; i < TEST_SMALL_VECTOR; i++) {
-        result[i] = vector_at(vector, &ptr, i);
-        assert_int_equal(result[i], SUCCESS);
-        assert_int_equal(*(int *)ptr, array[i]);
-    }
+//     for (uint32_t i = 0; i < TEST_SMALL_VECTOR; i++) {
+//         result[i] = vector_at(vector, &ptr, i);
+//         assert_int_equal(result[i], SUCCESS);
+//         assert_int_equal(*(int *)ptr, array[i]);
+//     }
 
-    vector_delete(&vector);
-}
+//     vector_delete(&vector);
+// }
 
-void print_int64_t(void *element, void *args) {
-    if (args != NULL) return;
-    printf("%ld\n", *(int64_t *)element);
-}
+// void print_int64_t(void *element, void *args) {
+//     if (args != NULL) return;
+//     printf("%ld\n", *(int64_t *)element);
+// }
 
-static void test_vector_iter() {
-    Vector vector = NULL;
-    int64_t array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-        16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-    };
+// static void test_vector_iter() {
+//     Vector vector = NULL;
+//     int64_t array[] = {
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//         16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+//     };
 
-    assert_int_equal(vector_from(&vector, array, TEST_SMALL_VECTOR, sizeof(int64_t)), SUCCESS);
-    assert_int_equal(vector_capacity(vector), TEST_SMALL_VECTOR);
-    assert_int_equal(vector_length(vector), TEST_SMALL_VECTOR);
+//     assert_int_equal(vector_from(&vector, array, TEST_SMALL_VECTOR, sizeof(int64_t)), SUCCESS);
+//     assert_int_equal(vector_capacity(vector), TEST_SMALL_VECTOR);
+//     assert_int_equal(vector_length(vector), TEST_SMALL_VECTOR);
 
-    // Buffer to capture the outputs
-    char vector_buffer[1024] = {0};
-    char array_buffer[1024] = {0};
+//     // Buffer to capture the outputs
+//     char vector_buffer[1024] = {0};
+//     char array_buffer[1024] = {0};
 
-    fflush(stdout);
-    int stdout_fd = dup(1);
-    int pipe_fd[2];
-    pipe(pipe_fd);
-    dup2(pipe_fd[1], 1);
-    close(pipe_fd[1]);
+//     fflush(stdout);
+//     int stdout_fd = dup(1);
+//     int pipe_fd[2];
+//     pipe(pipe_fd);
+//     dup2(pipe_fd[1], 1);
+//     close(pipe_fd[1]);
 
-    vector_iter(vector, print_int64_t, NULL);
+//     vector_iter(vector, print_int64_t, NULL);
 
-    fflush(stdout);
-    read(pipe_fd[0], vector_buffer, 1024);
-    vector_buffer[1024 - 1] = '\0';
-    dup2(stdout_fd, 1);
-    close(stdout_fd);
-    close(pipe_fd[0]);
+//     fflush(stdout);
+//     read(pipe_fd[0], vector_buffer, 1024);
+//     vector_buffer[1024 - 1] = '\0';
+//     dup2(stdout_fd, 1);
+//     close(stdout_fd);
+//     close(pipe_fd[0]);
 
 
-    fflush(stdout);
-    stdout_fd = dup(1);
-    pipe(pipe_fd);
-    dup2(pipe_fd[1], 1);
-    close(pipe_fd[1]);
+//     fflush(stdout);
+//     stdout_fd = dup(1);
+//     pipe(pipe_fd);
+//     dup2(pipe_fd[1], 1);
+//     close(pipe_fd[1]);
 
-    for (uint32_t j = 0; j < TEST_SMALL_VECTOR; j++)
-        print_int64_t((void *)&array[j], NULL);
+//     for (uint32_t j = 0; j < TEST_SMALL_VECTOR; j++)
+//         print_int64_t((void *)&array[j], NULL);
 
-    fflush(stdout);
-    read(pipe_fd[0], array_buffer, 1024);
-    array_buffer[1024 - 1] = '\0';
-    dup2(stdout_fd, 1);
-    close(stdout_fd);
-    close(pipe_fd[0]);
+//     fflush(stdout);
+//     read(pipe_fd[0], array_buffer, 1024);
+//     array_buffer[1024 - 1] = '\0';
+//     dup2(stdout_fd, 1);
+//     close(stdout_fd);
+//     close(pipe_fd[0]);
 
-    assert_string_equal(vector_buffer, array_buffer);
-    vector_delete(&vector);
-}
+//     assert_string_equal(vector_buffer, array_buffer);
+//     vector_delete(&vector);
+// }
 
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_vector_new),
-        cmocka_unit_test(test_vector_push),
-        cmocka_unit_test(test_vector_at),
-        cmocka_unit_test(test_vector_iter)
+        // cmocka_unit_test(test_vector_push),
+        // cmocka_unit_test(test_vector_at),
+        // cmocka_unit_test(test_vector_iter)
 
     };
 
