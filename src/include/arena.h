83cbe44f6817b0d78e99dd8b13da487843c26a74
daniqss <danielqueijo14@gmail.com>
2024-07-29 17:45:37 +0200

    change function declarations in arena.h

--- src/include/arena.h
+++ src/include/arena.h
@@ -6,9 +6,9 @@
 
 typedef struct _arena_t arena_t;
 
-result_t arena_new(arena_t ctx, uint64_t size);
-result_t arena_alloc_from(arena_t ctx, void *ptr, uint64_t size);
-result_t arena_delete(arena_t ctx);
+result_t arena_new(arena_t *ctx);
+result_t arena_alloc_from(arena_t *ctx, void *ptr, uint64_t size);
+result_t arena_delete(arena_t *ctx);
 
 
 #endif // ARENA_H
