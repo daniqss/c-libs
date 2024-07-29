2fc0d473ec8cca96c173d03f43885167e846f044
daniqss <danielqueijo14@gmail.com>
2024-07-29 17:07:56 +0200

    include result.h in memory.h

--- src/include/memory.h
+++ src/include/memory.h
@@ -2,6 +2,7 @@
 #define MEMORY_H
 
 #include <stdint.h>
+#include "result.h"
 
 result_t dump_memory(void *src, void **dest, uint32_t element_size);
 result_t copy_memory(void *src, void **dest, uint32_t element_size);
